8264c68995ad42c5f512105d225972aed66dfac6
SomeshMahajan <someshmahajan05@gmail.com>
2026-09-09 00:36:06 +0530

    Remove extra space in S3 bucket name

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "stairs-to-cloud.space "
+    bucket       = "stairs-to-cloud.space"
     key          = "student-management/eks/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
